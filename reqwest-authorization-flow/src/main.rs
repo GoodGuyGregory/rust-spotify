84920dee04de670c2a9d750d7320622511344a02
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-22 16:03:47 -0700

    adds target for removal

--- reqwest-authorization-flow/src/main.rs
+++ reqwest-authorization-flow/src/main.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 use std::iter;
-use axum::{response::IntoResponse, routing::get, Json, Router};
+use axum::{response::{IntoResponse, Redirect}, routing::get, Json, Router};
 
 // produce random string of characters:
 // https://stackoverflow.com/questions/54275459/how-do-i-create-a-random-string-by-sampling-from-alphanumeric-characters
@@ -25,19 +25,41 @@ async fn status_handler() -> impl IntoResponse{
     Json(json_response)
 }
 
-#[tokio::main]
-async fn main() {
-    // Documentation: https://developer.spotify.com/documentation/web-api/tutorials/code-flow
-    println!("Authorization Code Flow:");
+async fn get_spotify_login() -> impl IntoResponse {
 
+    println!("Server: Attempting Spotify Login 🔊");
 
-    // get the variables needed to make the authorization request
+     // get the variables needed to make the authorization request
     let client_credentials = dotenvy::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID must be set");
     let client_secret = dotenvy::var("SPOTIFY_CLIENT_SECRET").expect("SPOTIFY_CLIENT_SECRET must be set");
     let sp_callback = dotenvy::var("SPOTIFY_REDIRECT_URI").expect("REDIRECT URI must be set");
-
-    // produce a random string from the rand crate
+    
+     // produce a random string from the rand crate
     let state: String = random_string_generation(16);
+    
+    let query_params = vec![("response_type", "code"), 
+                            ("client_id", &client_credentials), 
+                            ("scope", "user-top-read"),
+                            ("redirect_uri", &sp_callback),
+                            ("state", &state)];
+
+    
+    // build the redirection string needed for spotify request
+    let mut authorize_route = String::from("https://accounts.spotify.com/authorize?");
+    authorize_route.push_str(&querystring::stringify(query_params));
+
+    Redirect::to(&authorize_route)
+
+}
+
+#[tokio::main]
+async fn main() {
+    // Documentation: https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+
+    // Code flow is all for this method: https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+
+    println!("Authorization Code Flow:");
+
 
     println!("🛸 Server started successfully");
 
@@ -45,7 +67,8 @@ async fn main() {
 
     // create the app
     let api_routes = Router::new()
-        .route("/status", get(status_handler));
+        .route("/status", get(status_handler))
+        .route("/spotify-auth", get(get_spotify_login));
 
     // return the router
     let app = Router::new().nest("/api", api_routes);

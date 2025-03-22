b52350112538560c80ec8f8c9739083cf00b0438
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-22 09:54:14 -0700

    adds documentation for the axum version

---
+++ axum-client-auth-spotify/readme.md
@@ -0,0 +1,8 @@
+## Spotify Authentication Flow with Axum
+
+### Documentation
+
+[Spotify-Authentication-Docs](https://developer.spotify.com/documentation/web-api/tutorials/code-flow)
+
+
+
---
+++ axum-client-auth-spotify/target/debug/deps/axum_client_auth_spotify-6386598c24c88535.d
@@ -0,0 +1,5 @@
+/Users/gregwitt/Github/rust-spotify/axum-client-auth-spotify/target/debug/deps/libaxum_client_auth_spotify-6386598c24c88535.rmeta: src/main.rs
+
+/Users/gregwitt/Github/rust-spotify/axum-client-auth-spotify/target/debug/deps/axum_client_auth_spotify-6386598c24c88535.d: src/main.rs
+
+src/main.rs:

60c8a1501efc84e7129b1c555f8aa4321d45424a
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-26 16:36:33 -0700

    adds cargo.toml changes

--- axum-client-auth-spotify/readme.md
+++
@@ -1,8 +0,0 @@
-## Spotify Authentication Flow with Axum
-
-### Documentation
-
-[Spotify-Authentication-Docs](https://developer.spotify.com/documentation/web-api/tutorials/code-flow)
-
-
-
--- axum-client-auth-spotify/target/debug/deps/axum_client_auth_spotify-f962278600f49b83.d
+++
@@ -1,5 +0,0 @@
-/Users/gregwitt/Github/rust-spotify/axum-client-auth-spotify/target/debug/deps/libaxum_client_auth_spotify-f962278600f49b83.rmeta: src/main.rs
-
-/Users/gregwitt/Github/rust-spotify/axum-client-auth-spotify/target/debug/deps/axum_client_auth_spotify-f962278600f49b83.d: src/main.rs
-
-src/main.rs:
